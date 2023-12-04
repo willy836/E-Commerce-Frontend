500878e988fcb90dc3ee0805cdb605180e7bfebf
willy836 <wilsonkimiri836@gmail.com>
2023-12-04 21:52:25 +0300

    Adjust responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <section className="py-5 px-20 bg-gray-900 text-gray-400 flex justify-between items-center">
+    <section className="py-5 px-5 sm:px-20 bg-gray-900 text-gray-400 flex flex-col gap-4 text-center lg:text-left lg:flex-row justify-between items-center">
       <div className="text-2xl font-bold">#TIDE</div>
       <div>
         <p className="uppercase">new to tide?</p>
         <p className="mb-2">
           Subscribe to our newsletter to get updates on our latest offers!
         </p>
-        <form className="w-full flex gap-2">
+        <form className="w-full flex justify-center lg:justify-start gap-2">
           <div className="w-2/3">
             <input
               type="text"
